a2ff7d11b9a79e00ae68f0dcadaedcd7b9ff7c1e
bmysoreshankar <bmysoreshankar@paypal.com>
2015-10-03 14:17:36 -0700

    sample spark java

---
+++ wrcapp/src/main/java/com/samaritan/house/wrcapp/spark/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.samaritan.house.wrcapp.spark;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.get;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello Spark MVC Framework!";
+            }
+        });
+    }
+}
