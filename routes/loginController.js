ec6b3e5730effd72fe051534c26347b7f8b00e46
Saideepak Budaraju <saideepak09@gmail.com>
2015-10-04 13:45:28 -0700

    Fixing login screen

--- routes/loginController.js
+++ routes/loginController.js
@@ -2,23 +2,27 @@ var mysql = require('./dbConnectionsController');
 
 exports.employerLogin = function(req, res){
 	var connection=mysql.getConnection();
-	var query = connection.query("Select * from LoginInfo where username = ? and Password=? ",[req.body.UserName, req.body.Password] , function(err, result){
+	var query = connection.query("Select * from LoginInfo where UserName = ? and Password=? ",[req.body.UserName, req.body.Password] , function(err, result){
 		if (err) {
-			res.error=err;									
+			console.log("Error: "+err);								
 		} 
 		else{
 			if((result[0].UserName == req.body.UserName) && (result[0].Password == req.body.UserName)){
-				req.session.UserName = login[0].UserName;
-				if(result[0].Role == "Employee"){
-					res.render('e'); 
-				}else if(result[0].Role == "Employee"){
-					res.render('e'); 
+				//req.session.UserName = login[0].UserName;
+				if(result[0].RolesID == "1"){
+					console.log("Reached Here. 1");
+					res.render('aHome'); 
+				}else if(result[0].RolesID == "2"){
+					console.log("Reached Here. 2"); 
+					res.render('eHome'); 
 				}else{
+					console.log("Reached Here. 3"); 
 					res.render('');
 				}
 			}
 			else{
-				res.render('/home');  //Change the URL.
+				console.log("Reached Here. 4"); 
+				res.render('');  //Change the URL.
 			}
 		}
 	});
